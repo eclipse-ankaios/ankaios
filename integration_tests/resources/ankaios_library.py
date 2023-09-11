9ebc74b432c47965b0abdf52e543c0da9b0253b5
lingnoi <chatree.akasarn@elektrobit.com>
2023-09-11 17:58:36 +0200

    added clean up functions
    added test with multiple agents

--- integration_tests/resources/ankaios_library.py
+++ integration_tests/resources/ankaios_library.py
@@ -1,23 +1,53 @@
-from robot.api.logger import info, debug, trace, console
-import subprocess, os
+import subprocess
+import time
 
-def ank_bin_dir():
-    ank_bin_path = os.environ["ANK_BIN_DIR"]
-    if ank_bin_path.endswith("/"):
-        return ank_bin_path
-    
-    return ank_bin_path + "/"
+def run_command(command, timeout=3):
+    try:
+        return subprocess.run(command, timeout=timeout, shell=True, executable='/bin/bash', check=True, capture_output=True, text=True)
+    except Exception as e:
+        print(f"execption!!! {e}")
+        return None
 
-def start_server(startup_config):
-    """
-    Start Ankaios server
-    """
-    print("Start Ankaios server")
-    args = [f"{ank_bin_dir()}ank-server", "--startup-config", startup_config]
-    process = subprocess.Popen(args)
-    
-    yield
+def table_to_list(raw):
+    raw = raw.strip()
+    splitted = raw.split('\n')
+    header = splitted.pop(0)
+    columns = []
+    index = 0
+    while index < (len(header) - 1):
+        while index < len(header) - 1 and not (header[index] == ' ' and header[index + 1] == ' '):
+            index += 1
+        while index < len(header) and header[index] == ' ':
+            index += 1
+        
+        columns.append(index)
+        index += 1
 
-    # process.kill()
+    table = []
+    for row in splitted:
+        last_column_index = 0
+        table_row = {}
+        for column_index in columns:
+            table_row[header[last_column_index: column_index].strip()] = row[last_column_index: column_index].strip()
+            last_column_index = column_index
+        table.append(table_row)
+
+    return table
+
+def wait_for_initial_execution_state(command, agent_name, timeout=10):
+        start_time = time.time()
+        print("exec run")
+        res = run_command(command)
+        table = table_to_list(res.stdout if res else "")
+        while (time.time() - start_time) < timeout:
+            if table and all([len(row["EXECUTION STATE"].strip()) > 0 for row in filter(lambda r: r["AGENT"] == agent_name, table)]):
+                return True
+
+            print("exec run")
+            time.sleep(1)
+            res = run_command(command)
+            table = table_to_list(res.stdout if res else "")
+        print("timeout!!!")
+        return False
 
 
--- integration_tests/tests/workloads.robot
+++ integration_tests/tests/workloads.robot
@@ -4,8 +4,16 @@ Resource         resources/ankaios.resource
 Suite Teardown    Terminate All Processes    kill=True
 
 *** Test Cases ***
+# my integration test test
 Test Ankaios CLI get workloads
+    # Preconditions
     Given Ankaios server is started with "/home/ubuntu/conoa/ankaios/server/resources/startConfig.yaml"
-    And Ankaios agent is started
+    And Ankaios agent is started with "ank-agent --name agent_B"
+    And all workloads of agent "agent_B" have an initial execution state
+    And Ankaios agent is started with "ank-agent --name agent_A"
+    And all workloads of agent "agent_A" have an initial execution state
+    # Actions
     When user triggers "ank get workloads"
-    Then result should be "my result"
+    # Assert
+    Then result should be "my result" 
+    [Teardown]    Clean up Ankaios
