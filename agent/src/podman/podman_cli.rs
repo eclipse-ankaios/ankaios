eclipse-ankaios/ankaios

commit 2420c47363762fa67270fd1b73c6f19d6b52e994

Move general CLI abstraction to extra module
Issue-Id: #1

agent/src/podman/podman_cli.rs
@@ -3,11 +3,13 @@ use tokio::io::AsyncWriteExt;
 
 use tokio::process::Command;
 
+use super::cli_command::CliCommand;
+
 const PODMAN_CMD: &str = "podman";
 
 pub async fn play_kube(kube_yml: &str) -> Result<String, String> {
-    let result = PodmanCliCommand::new()
-        .args(vec!["kube".into(), "play".into(), "-".into()])
+    let result = CliCommand::new(PODMAN_CMD)
+        .args(&["kube", "play", "-"])
         .stdin(kube_yml.as_bytes())
         .exec()
         .await?;
@@ -15,12 +17,12 @@ pub async fn play_kube(kube_yml: &str) -> Result<String, String> {
 }
 
 pub async fn list_workloads(regex: &str) -> Result<Vec<String>, String> {
-    let output = PodmanCliCommand::new()
-        .args(vec![
-            "ps".into(),
-            "--filter".into(),
-            format!("name={}", regex),
-            "--format={{.Names}}".into(),
+    let output = CliCommand::new(PODMAN_CMD)
+        .args(&[
+            "ps",
+            "--filter",
+            &format!("name={}", regex),
+            "--format={{.Names}}",
         ])
         .exec()
         .await?;
@@ -32,12 +34,12 @@ pub async fn list_workloads(regex: &str) -> Result<Vec<String>, String> {
 }
 
 pub async fn has_image(image_name: &str) -> Result<bool, String> {
-    let output = PodmanCliCommand::new()
-        .args(vec![
-            "images".into(),
-            "--filter".into(),
-            format!("reference={}", image_name),
-            "--format={{.Repository}}:{{.Tag}}".into(),
+    let output = CliCommand::new(PODMAN_CMD)
+        .args(&[
+            "images",
+            "--filter",
+            &format!("reference={}", image_name),
+            "--format={{.Repository}}:{{.Tag}}",
         ])
         .exec()
         .await?;
@@ -47,57 +49,5 @@ pub async fn has_image(image_name: &str) -> Result<bool, String> {
         .any(|x: String| x == *image_name))
 }
 
-struct PodmanCliCommand<'a> {
-    command: Command,
-    stdin: Option<&'a [u8]>,
-}
-
-impl<'a> PodmanCliCommand<'a> {
-    fn new() -> Self {
-        let mut command = Command::new(PODMAN_CMD);
-        command
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .stdin(Stdio::piped());
-        Self {
-            command,
-            stdin: None,
-        }
-    }
-
-    fn args(&mut self, args: Vec<String>) -> &mut Self {
-        self.command.args(args);
-        self
-    }
-
-    fn stdin(&mut self, stdin: &'a [u8]) -> &mut Self {
-        self.stdin = Some(stdin);
-        self
-    }
-
-    async fn exec(&mut self) -> Result<String, String> {
-        let mut child = self
-            .command
-            .spawn()
-            .map_err(|err| format!("Could not execute podman command: {}", err))?;
-
-        if let Some(stdin) = self.stdin {
-            child
-                .stdin
-                .as_mut()
-                .ok_or_else(|| "Could not access podman stdin".to_string())?
-                .write_all(stdin)
-                .await
-                .map_err(|err| format!("Could write data to podman command: {}", err))?;
-        }
-        let result = child.wait_with_output().await.unwrap();
-        if result.status.success() {
-            String::from_utf8(result.stdout)
-                .map_err(|err| format!("Could not decode podman output as UTF8: {}", err))
-        } else {
-            let stderr = String::from_utf8(result.stderr)
-                .unwrap_or_else(|err| format!("Could not decode podman stderr as UTF8: {}", err));
-            Err(format!("Execution of podman failed: {}", stderr))
-        }
     }
 }

agent/src/podman/cli_command.rs
@@ -1,0 +1,149 @@
+use std::process::Stdio;
+use tokio::io::AsyncWriteExt;
+use tokio::process::Command;
+
+pub struct CliCommand<'a> {
+    command: Command,
+    stdin: Option<&'a [u8]>,
+}
+
+impl<'a> CliCommand<'a> {
+    pub fn new(program: &str) -> Self {
+        let mut command = Command::new(program);
+        command
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .stdin(Stdio::piped());
+        Self {
+            command,
+            stdin: None,
+        }
+    }
+
+    pub fn args(&mut self, args: &[&str]) -> &mut Self {
+        self.command.args(args);
+        self
+    }
+
+    pub fn stdin(&mut self, stdin: &'a [u8]) -> &mut Self {
+        self.stdin = Some(stdin);
+        self
+    }
+
+    pub async fn exec(&mut self) -> Result<String, String> {
+        let mut child = self
+            .command
+            .spawn()
+            .map_err(|err| format!("Could not execute command: {}", err))?;
+
+        if let Some(stdin) = self.stdin {
+            child
+                .stdin
+                .as_mut()
+                .ok_or_else(|| "Could not access commands stdin".to_string())?
+                .write_all(stdin)
+                .await
+                .map_err(|err| format!("Could write data to command: {}", err))?;
+        }
+        let result = child.wait_with_output().await.unwrap();
+        if result.status.success() {
+            String::from_utf8(result.stdout)
+                .map_err(|err| format!("Could not decode command's output as UTF8: {}", err))
+        } else {
+            let stderr = String::from_utf8(result.stderr).unwrap_or_else(|err| {
+                format!("Could not decode command's stderr as UTF8: {}", err)
+            });
+            Err(format!("Execution of command failed: {}", stderr))
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//                 ########  #######    #########  #########                //
+//                    ##     ##        ##             ##                    //
+//                    ##     #####     #########      ##                    //
+//                    ##     ##                ##     ##                    //
+//                    ##     #######   #########      ##                    //
+//////////////////////////////////////////////////////////////////////////////
+#[cfg(test)]
+mod tests {
+    use super::CliCommand;
+
+    #[tokio::test]
+    async fn utest_cli_command_simple_output() {
+        let result = CliCommand::new("echo")
+            .args(&["Hello", "World"])
+            .exec()
+            .await;
+        assert!(matches!(result, Ok(x) if x.eq("Hello World\n")))
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_simple_output_multiple_args() {
+        let result = CliCommand::new("echo")
+            .args(&["Hello"])
+            .args(&["World"])
+            .exec()
+            .await;
+        assert!(matches!(result, Ok(x) if x.eq("Hello World\n")));
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_fail_on_not_existing_command() {
+        let result = CliCommand::new("non_existing_command").exec().await;
+        assert!(matches!(result, Err(x) if x.starts_with("Could not execute command")));
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_simple_input_output() {
+        let result = CliCommand::new("tr")
+            .args(&["[:lower:]", "[:upper:]"])
+            .stdin("Hello World".as_bytes())
+            .exec()
+            .await;
+        assert!(matches!(result, Ok(x) if x.eq("HELLO WORLD")));
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_only_forward_stdout() {
+        let result = CliCommand::new("bash")
+            .args(&["-c", "echo output;echo error >&2"])
+            .exec()
+            .await;
+
+        assert!(matches!(result, Ok(x) if x== "output\n"));
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_on_fail_only_forward_stderr() {
+        let result = CliCommand::new("bash")
+            .args(&["-c", "echo output;echo error >&2; false"])
+            .exec()
+            .await;
+
+        assert!(matches!(result, Err(x) if x== "Execution of command failed: error\n"));
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_stdout_not_utf8() {
+        let result = CliCommand::new("bash")
+            .args(&["-c", "echo wAA= | base64 -d"])
+            .exec()
+            .await;
+        assert!(
+            matches!(result, Err(x) if x.starts_with("Could not decode command's output as UTF8"))
+        );
+    }
+
+    #[tokio::test]
+    async fn utest_cli_command_stderr_not_utf8() {
+        let result = CliCommand::new("bash")
+            .args(&["-c", "echo wAA= | base64 -d >&2; false"])
+            .exec()
+            .await;
+
+        assert!(
+            matches!(result, Err(x) if x.starts_with("Execution of command failed: Could not decode command's stderr as UTF8"))
+        );
+    }
+}

agent/src/podman/mod.rs
@@ -17,6 +17,7 @@ mod podman_cli;
 mod podman_kube_runtime;
 mod podman_runtime;
 pub use podman_runtime::{PodmanConfig, PodmanRuntime, PodmanWorkloadId};
+mod cli_command;
 mod podman_runtime_config;
 mod podman_utils;
 mod workload;

commit a71c0a570c5f24605ea03bf90f36a99785255e22

Fix the build break introduced by 2420c47
Issue-Id: #1

agent/src/podman/podman_cli.rs
@@ -48,6 +48,3 @@ pub async fn has_image(image_name: &str) -> Result<bool, String> {
         .map(|x| x.trim().into())
         .any(|x: String| x == *image_name))
 }
-
-    }
-}