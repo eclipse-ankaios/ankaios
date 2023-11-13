b33e1e901bd7c3e5af963bc976cc4192a42034e8
Oliver Klapper <oliver.klapper@elektrobit.com>
2023-11-13 13:46:19 +0100

    Revert changes
    Issue-Id: #75

--- ank/src/cli.rs
+++ ank/src/cli.rs
@@ -12,80 +12,14 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{env, error::Error, str::FromStr};
+use std::error::Error;
 
-use clap::{
-    command,
-    error::{ContextKind, ContextValue, ErrorKind},
-    Parser, Subcommand,
-};
+use clap::{command, Parser, Subcommand};
 
 use common::DEFAULT_SERVER_ADDRESS;
 use url::Url;
 
 const ANK_SERVER_URL_ENV_KEY: &str = "ANK_SERVER_URL";
-
-fn create_error_context(
-    cmd: &clap::Command,
-    env_key: &str,
-    arg: Option<&clap::Arg>,
-    arg_value: &String,
-) -> clap::Error {
-    let mut err = clap::Error::new(ErrorKind::ValueValidation).with_cmd(cmd); // the order in which the errors are inserted is important
-    if let Some(arg) = arg {
-        if let Ok(env_value) = env::var(env_key) {
-            if env_value == *arg_value {
-                err.insert(
-                    ContextKind::InvalidArg,
-                    ContextValue::String(format!("environment variable '{}'", env_key)),
-                );
-            } else {
-                err.insert(
-                    ContextKind::InvalidArg,
-                    ContextValue::String(arg.to_string()),
-                );
-            }
-        } else {
-            err.insert(
-                ContextKind::InvalidArg,
-                ContextValue::String(arg.to_string()),
-            );
-        }
-    }
-
-    err.insert(
-        ContextKind::InvalidValue,
-        ContextValue::String(arg_value.clone()),
-    );
-    err
-}
-
-#[derive(Clone)]
-/// Custom url parser as a workaround to Clap's bug about
-/// outputting the wrong error message context
-/// when using the environment variable and not the cli argument.
-/// When using a wrong value inside environment variable
-/// Clap still outputs that the cli argument was wrongly set,
-/// but not the environment variable. This is poor usability.
-/// An issue for this bug is already opened (https://github.com/clap-rs/clap/issues/5202).
-/// The code will be removed when the bug in Clap is fixed.
-pub struct ServerUrlParser;
-
-impl clap::builder::TypedValueParser for ServerUrlParser {
-    type Value = Url;
-
-    fn parse_ref(
-        &self,
-        cmd: &clap::Command,
-        arg: Option<&clap::Arg>,
-        value: &std::ffi::OsStr,
-    ) -> Result<Self::Value, clap::Error> {
-        let arg_value = value.to_string_lossy().to_string();
-        let err = create_error_context(cmd, ANK_SERVER_URL_ENV_KEY, arg, &arg_value);
-        let url = Url::from_str(&arg_value).map_err(|_| err)?;
-        Ok(url)
-    }
-}
 
 // [impl->swdd~cli-shall-support-environment-variables~1]
 // [impl->swdd~cli-prioritizes-cli-argument-over-environment-variable~1]
@@ -97,7 +31,7 @@ impl clap::builder::TypedValueParser for ServerUrlParser {
 pub struct AnkCli {
     #[command(subcommand)]
     pub command: Commands,
-    #[clap(short = 's', long = "server-url", default_value_t = DEFAULT_SERVER_ADDRESS.parse().unwrap(), env = ANK_SERVER_URL_ENV_KEY, value_parser = ServerUrlParser)]
+    #[clap(short = 's', long = "server-url", default_value_t = DEFAULT_SERVER_ADDRESS.parse().unwrap(), env = ANK_SERVER_URL_ENV_KEY)]
     /// The url to Ankaios server.
     pub server_url: Url,
     #[clap(long = "response-timeout", default_value_t = 3000)]
@@ -251,123 +185,4 @@ where
 
 pub fn parse() -> AnkCli {
     AnkCli::parse()
-}
-
-//////////////////////////////////////////////////////////////////////////////
-//                 ########  #######    #########  #########                //
-//                    ##     ##        ##             ##                    //
-//                    ##     #####     #########      ##                    //
-//                    ##     ##                ##     ##                    //
-//                    ##     #######   #########      ##                    //
-//////////////////////////////////////////////////////////////////////////////
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use clap::builder::TypedValueParser;
-    use std::env;
-    use std::ffi::OsStr;
-    const INVALID_VALUE: &str = "invalid-value";
-    const PROGRAM_NAME: &str = "some program";
-    const ARG_NAME: &str = "arg";
-    const EXAMPLE_URL: &str = "http://0.0.0.0:11111";
-
-    struct CleanupEnv;
-
-    impl Drop for CleanupEnv {
-        fn drop(&mut self) {
-            env::remove_var(ANK_SERVER_URL_ENV_KEY);
-        }
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_use_cli_arg() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let arg = clap::Arg::new(ARG_NAME);
-        let actual_url = url_parser
-            .parse_ref(&cmd, Some(&arg), OsStr::new(common::DEFAULT_SERVER_ADDRESS))
-            .unwrap();
-        let expected_url = Url::from_str(common::DEFAULT_SERVER_ADDRESS).unwrap();
-        assert_eq!(actual_url, expected_url);
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_use_env_var() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let arg = clap::Arg::new(ARG_NAME);
-        std::env::set_var(ANK_SERVER_URL_ENV_KEY, EXAMPLE_URL);
-        let actual_url = url_parser
-            .parse_ref(&cmd, Some(&arg), OsStr::new(EXAMPLE_URL))
-            .unwrap();
-
-        let expected_url = Url::from_str(EXAMPLE_URL).unwrap();
-        assert_eq!(actual_url, expected_url);
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_prioritize_cli_arg() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let arg = clap::Arg::new(ARG_NAME);
-        std::env::set_var(ANK_SERVER_URL_ENV_KEY, EXAMPLE_URL);
-        let actual_url = url_parser
-            .parse_ref(&cmd, Some(&arg), OsStr::new(common::DEFAULT_SERVER_ADDRESS))
-            .unwrap();
-
-        let expected_url = Url::from_str(common::DEFAULT_SERVER_ADDRESS).unwrap();
-        assert_eq!(actual_url, expected_url);
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_use_env_var_error_context() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let arg = clap::Arg::new(ARG_NAME);
-        std::env::set_var(ANK_SERVER_URL_ENV_KEY, INVALID_VALUE);
-        let parsing_result = url_parser.parse_ref(&cmd, Some(&arg), OsStr::new(INVALID_VALUE));
-
-        assert!(parsing_result
-            .err()
-            .unwrap()
-            .to_string()
-            .contains("environment variable"));
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_use_cli_arg_error_context() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let arg = clap::Arg::new(ARG_NAME);
-        let parsing_result = url_parser.parse_ref(&cmd, Some(&arg), OsStr::new(INVALID_VALUE));
-        assert!(parsing_result.err().unwrap().to_string().contains(ARG_NAME));
-    }
-
-    // [utest->swdd~cli-shall-support-environment-variables~1]
-    #[test]
-    fn utest_cli_argument_server_url_none_arg_error_context() {
-        let _guard = crate::test_helper::MOCKALL_CONTEXT_SYNC.get_lock();
-        let _cleanup = CleanupEnv;
-        let url_parser = ServerUrlParser;
-        let cmd = clap::Command::new(PROGRAM_NAME);
-        let parsing_result = url_parser.parse_ref(&cmd, None, OsStr::new(INVALID_VALUE));
-        let err: String = parsing_result.err().unwrap().to_string();
-        assert!(err.contains("invalid value for one of the arguments"));
-    }
 }
