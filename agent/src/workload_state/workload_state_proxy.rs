f295467ce3bfea988f7c252a02b1afc0971f3b25
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-01-16 13:05:21 +0000

    Unit tests in state proxy
    Issue-Id: #48

--- agent/src/workload_state/workload_state_proxy.rs
+++ agent/src/workload_state/workload_state_proxy.rs
@@ -195,7 +195,7 @@ mod tests {
     const BUFFER_SIZE: usize = 20;
 
     #[tokio::test]
-    async fn utest_workload_state_proxy_start_stores_from_checker_and_forwards() {
+    async fn utest_workload_state_proxy_start_from_checker_stores_and_forwards() {
         let (workload_state_sender, proxy_receiver) =
             tokio::sync::mpsc::channel::<WorkloadStateMessage>(BUFFER_SIZE);
         let (to_server, mut server_receiver) =
@@ -233,5 +233,158 @@ mod tests {
             Some(&ExecutionState::ExecStarting)
         );
     }
-    // TODO write tests
+
+    #[tokio::test]
+    async fn utest_workload_state_proxy_start_from_checker_transforms_stores_and_forwards_remains()
+    {
+        let (workload_state_sender, proxy_receiver) =
+            tokio::sync::mpsc::channel::<WorkloadStateMessage>(BUFFER_SIZE);
+        let (to_server, mut server_receiver) =
+            tokio::sync::mpsc::channel::<StateChangeCommand>(BUFFER_SIZE);
+
+        let mut test_workload_state_proxy =
+            WorkloadStateProxy::new(to_server.clone(), proxy_receiver);
+
+        let workload_spec = generate_test_workload_spec();
+        let instance_name = workload_spec.instance_name();
+
+        let initial_state = WorkloadState {
+            workload_name: instance_name.workload_name().to_string(),
+            agent_name: instance_name.agent_name().to_string(),
+            execution_state: ExecutionState::ExecStopping,
+        };
+
+        let mut expected_state = initial_state.clone();
+        expected_state.execution_state = ExecutionState::ExecRunning;
+
+        let workload_states = vec![initial_state.clone()];
+
+        // Fill the state store with a value
+        test_workload_state_proxy
+            .states_db
+            .update_workload_state(workload_states[0].clone());
+
+        workload_state_sender
+            .report_workload_execution_state(
+                instance_name.workload_name().to_string(),
+                instance_name.agent_name().to_string(),
+                ExecutionState::ExecRunning,
+            )
+            .await
+            .unwrap();
+
+        drop(workload_state_sender);
+
+        test_workload_state_proxy.start().await;
+
+        let result = server_receiver.recv().await.unwrap();
+
+        assert!(matches!(
+            result,
+            StateChangeCommand::UpdateWorkloadState(common::commands::UpdateWorkloadState{workload_states})
+            if workload_states == vec![initial_state.clone()]));
+
+        assert_eq!(
+            test_workload_state_proxy
+                .states_db
+                .get_state_of_workload(instance_name.workload_name()),
+            Some(&initial_state.execution_state)
+        );
+    }
+
+    #[tokio::test]
+    async fn utest_workload_state_proxy_start_from_checker_transforms_stores_and_forwards_changed()
+    {
+        let (workload_state_sender, proxy_receiver) =
+            tokio::sync::mpsc::channel::<WorkloadStateMessage>(BUFFER_SIZE);
+        let (to_server, mut server_receiver) =
+            tokio::sync::mpsc::channel::<StateChangeCommand>(BUFFER_SIZE);
+
+        let mut test_workload_state_proxy =
+            WorkloadStateProxy::new(to_server.clone(), proxy_receiver);
+
+        let workload_spec = generate_test_workload_spec();
+        let instance_name = workload_spec.instance_name();
+
+        let initial_state = WorkloadState {
+            workload_name: instance_name.workload_name().to_string(),
+            agent_name: instance_name.agent_name().to_string(),
+            execution_state: ExecutionState::ExecStarting,
+        };
+
+        let mut expected_state = initial_state.clone();
+        expected_state.execution_state = ExecutionState::ExecRunning;
+
+        let workload_states = vec![initial_state.clone()];
+
+        // Fill the state store with a value
+        test_workload_state_proxy
+            .states_db
+            .update_workload_state(workload_states[0].clone());
+
+        workload_state_sender
+            .report_workload_execution_state(
+                instance_name.workload_name().to_string(),
+                instance_name.agent_name().to_string(),
+                ExecutionState::ExecRunning,
+            )
+            .await
+            .unwrap();
+
+        drop(workload_state_sender);
+
+        test_workload_state_proxy.start().await;
+
+        let result = server_receiver.recv().await.unwrap();
+
+        assert!(matches!(
+            result,
+            StateChangeCommand::UpdateWorkloadState(common::commands::UpdateWorkloadState{workload_states})
+            if workload_states == vec![expected_state.clone()]));
+
+        assert_eq!(
+            test_workload_state_proxy
+                .states_db
+                .get_state_of_workload(instance_name.workload_name()),
+            Some(&expected_state.execution_state)
+        );
+    }
+
+    #[tokio::test]
+    async fn utest_workload_state_proxy_start_stores_from_server() {
+        let (workload_state_sender, proxy_receiver) =
+            tokio::sync::mpsc::channel::<WorkloadStateMessage>(BUFFER_SIZE);
+        let (to_server, mut server_receiver) =
+            tokio::sync::mpsc::channel::<StateChangeCommand>(BUFFER_SIZE);
+
+        let mut test_workload_state_proxy =
+            WorkloadStateProxy::new(to_server.clone(), proxy_receiver);
+
+        let workload_spec = generate_test_workload_spec();
+        let instance_name = workload_spec.instance_name();
+
+        let remote_states = vec![WorkloadState {
+            workload_name: instance_name.workload_name().to_string(),
+            agent_name: instance_name.agent_name().to_string(),
+            execution_state: ExecutionState::ExecStarting,
+        }];
+
+        workload_state_sender
+            .store_remote_workload_states(remote_states)
+            .await
+            .unwrap();
+
+        drop(workload_state_sender);
+
+        test_workload_state_proxy.start().await;
+
+        assert!(server_receiver.try_recv().is_err());
+
+        assert_eq!(
+            test_workload_state_proxy
+                .states_db
+                .get_state_of_workload(instance_name.workload_name()),
+            Some(&ExecutionState::ExecStarting)
+        );
+    }
 }
