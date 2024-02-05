6d840dcd94aea65c83f22979aff087354c84edc6
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-02-05 15:24:26 +0000

    Fix workload state db + utests

--- server/src/workload_state_db.rs
+++ server/src/workload_state_db.rs
@@ -58,13 +58,13 @@ impl WorkloadStateDB {
     }
 
     pub fn agent_disconnected(&mut self, agent_name: &str) {
-        self.stored_states.get_mut(agent_name).map(|x| {
-            x.values_mut().map(|y| {
-                y.iter_mut().for_each(|wl_state| {
+        if let Some(agent_states) = self.stored_states.get_mut(agent_name) {
+            agent_states.iter_mut().for_each(|(_, states)| {
+                states.iter_mut().for_each(|wl_state| {
                     wl_state.execution_state = ExecutionState::agent_disconnected()
                 })
             })
-        });
+        }
     }
 
     pub fn insert(&mut self, workload_states: Vec<WorkloadState>) {
@@ -96,117 +96,126 @@ impl Default for WorkloadStateDB {
 mod tests {
     use std::collections::HashMap;
 
-    use common::objects::{ExecutionState, WorkloadState};
+    use common::objects::{generate_test_workload_state_with_agent, ExecutionState};
 
     use super::WorkloadStateDB;
 
-    fn create_test_setup_1(agent_name: &str) -> WorkloadStateDB {
-        let mut wls_db = WorkloadStateDB::new();
-        let mut wls = HashMap::new();
-        wls.insert("workload1".to_owned(), ExecutionState::ExecSucceeded);
-        wls.insert("workload2".to_owned(), ExecutionState::ExecStarting);
-        wls_db.stored_states.insert(agent_name.to_string(), wls);
-        wls_db
-    }
+    const AGENT_A: &str = "agent_A";
+    const AGENT_B: &str = "agent_B";
+    const WORKLOAD_NAME_1: &str = "workload_1";
+    const WORKLOAD_NAME_2: &str = "workload_2";
+    const WORKLOAD_NAME_3: &str = "workload_3";
 
-    fn create_test_setup_2(agent_name_1: &str, agent_name_2: &str) -> WorkloadStateDB {
+    fn create_test_setup() -> WorkloadStateDB {
         let mut wls_db = WorkloadStateDB::new();
 
+        let wl_1_state = generate_test_workload_state_with_agent(
+            WORKLOAD_NAME_1,
+            AGENT_A,
+            ExecutionState::succeeded(),
+        );
+        let wl_2_state = generate_test_workload_state_with_agent(
+            WORKLOAD_NAME_2,
+            AGENT_A,
+            ExecutionState::starting("additional_info"),
+        );
+        let wl_3_state = generate_test_workload_state_with_agent(
+            WORKLOAD_NAME_3,
+            AGENT_B,
+            ExecutionState::running(),
+        );
+
         let mut wls = HashMap::new();
-        wls.insert("workload1".to_owned(), ExecutionState::ExecSucceeded);
-        wls.insert("workload2".to_owned(), ExecutionState::ExecStarting);
-        wls_db.stored_states.insert(agent_name_1.to_string(), wls);
+        wls.insert(
+            wl_1_state.instance_name.workload_name().to_owned(),
+            vec![wl_1_state],
+        );
+        wls.insert(
+            wl_2_state.instance_name.workload_name().to_owned(),
+            vec![wl_2_state],
+        );
+        wls_db.stored_states.insert(AGENT_A.to_string(), wls);
 
         let mut wls_2 = HashMap::new();
-        wls_2.insert("workload3".to_owned(), ExecutionState::ExecRunning);
-        wls_db.stored_states.insert(agent_name_2.to_string(), wls_2);
+        wls_2.insert(
+            wl_3_state.instance_name.workload_name().to_owned(),
+            vec![wl_3_state],
+        );
+        wls_db.stored_states.insert(AGENT_B.to_string(), wls_2);
 
         wls_db
     }
 
     #[test]
     fn utest_get_all_workload_states_returns_correct() {
-        let agent_name_1 = "test_agent_1";
-        let agent_name_2 = "test_agent_2";
-        let wls_db = create_test_setup_2(agent_name_1, agent_name_2);
+        let wls_db = create_test_setup();
 
         let mut wls_res = wls_db.get_all_workload_states();
-        wls_res.sort_by(|a, b| a.workload_name.cmp(&b.workload_name));
+        wls_res.sort_by(|a, b| {
+            a.instance_name
+                .workload_name()
+                .cmp(b.instance_name.workload_name())
+        });
 
         assert_eq!(
             wls_res,
             vec![
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload1".to_string(),
-                    execution_state: ExecutionState::ExecSucceeded
-                },
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload2".to_string(),
-                    execution_state: ExecutionState::ExecStarting
-                },
-                WorkloadState {
-                    agent_name: agent_name_2.to_string(),
-                    workload_name: "workload3".to_string(),
-                    execution_state: ExecutionState::ExecRunning
-                }
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_1,
+                    AGENT_A,
+                    ExecutionState::succeeded()
+                ),
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_2,
+                    AGENT_A,
+                    ExecutionState::starting("additional_info"),
+                ),
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_3,
+                    AGENT_B,
+                    ExecutionState::running()
+                ),
             ]
         )
     }
 
     #[test]
     fn utest_mark_all_workload_state_for_agent_unknown() {
-        let agent_name_1 = "test_agent_1";
-        let agent_name_2 = "test_agent_2";
-        let mut wls_db = create_test_setup_2(agent_name_1, agent_name_2);
+        let mut wls_db = create_test_setup();
 
         let mut wls_res = wls_db.get_all_workload_states();
-        wls_res.sort_by(|a, b| a.workload_name.cmp(&b.workload_name));
+        wls_res.sort_by(|a, b| {
+            a.instance_name
+                .workload_name()
+                .cmp(b.instance_name.workload_name())
+        });
 
-        assert_eq!(
-            wls_res,
-            vec![
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload1".to_string(),
-                    execution_state: ExecutionState::ExecSucceeded
-                },
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload2".to_string(),
-                    execution_state: ExecutionState::ExecStarting
-                },
-                WorkloadState {
-                    agent_name: agent_name_2.to_string(),
-                    workload_name: "workload3".to_string(),
-                    execution_state: ExecutionState::ExecRunning
-                }
-            ]
-        );
-
-        wls_db.agent_disconnected(agent_name_1);
+        wls_db.agent_disconnected(AGENT_A);
         let mut wls_res_marked = wls_db.get_all_workload_states();
-        wls_res_marked.sort_by(|a, b| a.workload_name.cmp(&b.workload_name));
+        wls_res_marked.sort_by(|a, b| {
+            a.instance_name
+                .workload_name()
+                .cmp(b.instance_name.workload_name())
+        });
 
         assert_eq!(
             wls_res_marked,
             vec![
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload1".to_string(),
-                    execution_state: ExecutionState::ExecUnknown
-                },
-                WorkloadState {
-                    agent_name: agent_name_1.to_string(),
-                    workload_name: "workload2".to_string(),
-                    execution_state: ExecutionState::ExecUnknown
-                },
-                WorkloadState {
-                    agent_name: agent_name_2.to_string(),
-                    workload_name: "workload3".to_string(),
-                    execution_state: ExecutionState::ExecRunning
-                }
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_1,
+                    AGENT_A,
+                    ExecutionState::agent_disconnected()
+                ),
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_2,
+                    AGENT_A,
+                    ExecutionState::agent_disconnected()
+                ),
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_3,
+                    AGENT_B,
+                    ExecutionState::running()
+                ),
             ]
         )
     }
@@ -214,31 +223,35 @@ mod tests {
     #[test]
     fn utest_get_workload_state_for_agent_returns_workload_state_of_existing_agent_name() {
         let agent_name = "test_agent";
-        let wls_db = create_test_setup_1(agent_name);
+        let wls_db = create_test_setup();
 
-        let mut wls_res = wls_db.get_workload_state_for_agent(agent_name);
-        wls_res.sort_by(|a, b| a.workload_name.cmp(&b.workload_name));
+        let mut wls_res = wls_db.get_workload_state_for_agent(AGENT_A);
+        wls_res.sort_by(|a, b| {
+            a.instance_name
+                .workload_name()
+                .cmp(b.instance_name.workload_name())
+        });
 
         assert_eq!(
             wls_res,
             vec![
-                WorkloadState {
-                    agent_name: agent_name.to_string(),
-                    workload_name: "workload1".to_string(),
-                    execution_state: ExecutionState::ExecSucceeded
-                },
-                WorkloadState {
-                    agent_name: agent_name.to_string(),
-                    workload_name: "workload2".to_string(),
-                    execution_state: ExecutionState::ExecStarting
-                }
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_1,
+                    AGENT_A,
+                    ExecutionState::succeeded()
+                ),
+                generate_test_workload_state_with_agent(
+                    WORKLOAD_NAME_2,
+                    AGENT_A,
+                    ExecutionState::starting("additional_info"),
+                ),
             ]
         )
     }
 
     #[test]
     fn utest_get_workload_state_for_agent_returns_empty_list_of_non_existing_agent_name() {
-        let wls_db = create_test_setup_1("test_agent");
+        let wls_db = create_test_setup();
         assert_eq!(
             wls_db.get_workload_state_for_agent("non_existing_agent"),
             vec![]
