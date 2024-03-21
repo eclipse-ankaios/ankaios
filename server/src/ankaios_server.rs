0e4a5f7c81417eba45eda1b19c52c7db447b7a78
Rostislav <35850677+maturar@users.noreply.github.com>
2024-03-21 10:23:16 +0000

    Add unit test for invalid version in startupCfg
    Issue-Id: #208

--- server/src/ankaios_server.rs
+++ server/src/ankaios_server.rs
@@ -449,9 +449,35 @@ mod tests {
         server.server_state = mock_server_state;
 
         let result = server.start(Some(startup_state)).await;
-        assert!(result.is_err());
+        assert_eq!(
+            result,
+            Err("workload dependency 'workload_A part of cycle.' is part of a cycle.".into())
+        );
 
         assert!(comm_middle_ware_receiver.try_recv().is_err());
+    }
+
+    // [utest->swdd~server-fails-on-invalid-startup-state~1]
+    #[tokio::test]
+    async fn utest_server_start_fail_on_startup_config_with_invalid_version() {
+        let (_to_server, server_receiver) = create_to_server_channel(common::CHANNEL_CAPACITY);
+        let (to_agents, _comm_middle_ware_receiver) =
+            create_from_server_channel(common::CHANNEL_CAPACITY);
+
+        let startup_state = CompleteState {
+            desired_state: State {
+                api_version: "invalidVersion".into(),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let mut server = AnkaiosServer::new(server_receiver, to_agents);
+        let result = server.start(Some(startup_state)).await;
+        assert_eq!(
+            result,
+            Err("Unsupported API version. Received 'invalidVersion', expected 'v0.1'".into())
+        );
     }
 
     // [utest->swdd~server-continues-on-invalid-updated-state~1]
