2c4cc92f3b38c4605ef01a624f5baf9e0e132bfa
Christoph Hamm <130038849+christoph-hamm@users.noreply.github.com>
2024-07-26 12:37:53 +0200

    Add utests for authorizer::rule
    Issue-Id: #22

--- agent/src/control_interface/authorizer/rule.rs
+++ agent/src/control_interface/authorizer/rule.rs
@@ -40,3 +40,73 @@ impl<P: PathPattern> PathPattern for Rule<P> {
             .unwrap_or_else(|| (false, String::new()))
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//                 ########  #######    #########  #########                //
+//                    ##     ##        ##             ##                    //
+//                    ##     #####     #########      ##                    //
+//                    ##     ##                ##     ##                    //
+//                    ##     #######   #########      ##                    //
+//////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod test {
+    use super::{super::path::Path, Rule};
+    use crate::control_interface::authorizer::path_pattern::{PathPattern, PathPatternMatchReason};
+
+    struct MockPathPattern {
+        matching_path: Path,
+        reason: String,
+    }
+
+    impl MockPathPattern {
+        fn create(pattern: &str, reason: &str) -> Self {
+            Self {
+                matching_path: pattern.into(),
+                reason: reason.into(),
+            }
+        }
+    }
+
+    impl PathPattern for MockPathPattern {
+        fn matches(&self, other: &Path) -> (bool, PathPatternMatchReason) {
+            if other.sections == self.matching_path.sections {
+                (true, self.reason.clone())
+            } else {
+                (false, String::new())
+            }
+        }
+    }
+
+    #[test]
+    fn utest_empty_rule() {
+        let rule = Rule::<MockPathPattern>::create(Vec::new());
+        assert_eq!(rule.matches(&Path::from("some.path")), (false, "".into()));
+    }
+
+    #[test]
+    fn utest_matches_on_pattern_in_rule() {
+        let rule = Rule::create(vec![
+            MockPathPattern::create("pattern.1", "reason1"),
+            MockPathPattern::create("pattern.2", "reason2"),
+            MockPathPattern::create("pattern.3", "reason3"),
+        ]);
+        assert_eq!(
+            rule.matches(&Path::from("pattern.2")),
+            (true, "reason2".into())
+        );
+    }
+
+    #[test]
+    fn utest_matches_none_pattern_in_rule() {
+        let rule = Rule::create(vec![
+            MockPathPattern::create("pattern.1", "reason1"),
+            MockPathPattern::create("pattern.2", "reason2"),
+            MockPathPattern::create("pattern.3", "reason3"),
+        ]);
+        assert_eq!(
+            rule.matches(&Path::from("no.matching.pattern")),
+            (false, "".into())
+        );
+    }
+}
