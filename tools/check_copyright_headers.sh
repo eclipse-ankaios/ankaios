acad35046f0e4d1a4bd9ee5297a22fa4e156b1bc
Tomuta Gabriel <gabriel.tomuta@elektrobit.com>
2024-09-30 13:31:10 +0300

    Simplify check copyright script

--- tools/check_copyright_headers.sh
+++ tools/check_copyright_headers.sh
@@ -15,13 +15,8 @@ EXCLUDE_PATHS=(
 
 function check_copyright_headers() {
     local file_ext_cmd=""
-    for ext in "${FILE_EXT[@]}"; do
-        if [ -n "$file_ext_cmd" ]; then
-            file_ext_cmd="$file_ext_cmd -o -name \"$ext\""
-        else
-            file_ext_cmd="-name \"$ext\""
-        fi
-    done
+    file_ext_cmd=$(printf " -o -name %s" "${FILE_EXT[@]}")
+    file_ext_cmd="${file_ext_cmd:4}"  # Remove the leading ' -o'
 
     local exclude_paths_cmd=""
     for path in "${EXCLUDE_PATHS[@]}"; do
@@ -29,26 +24,18 @@ function check_copyright_headers() {
     done
 
     local cmd="find \"$SEARCH_DIR\" -type f $file_ext_cmd $exclude_paths_cmd -print"
-    local no_copyright_count=0
+    missing_files=$(eval "$cmd" | xargs grep -L -E "$COPYRIGHT_REGEX")
 
-    while IFS= read -r file; do
-        # Check if the copyright header is present in the file
-        if ! grep -E -q "$COPYRIGHT_REGEX" "$file"; then
-            echo "No copyright header found in: $file"
-            no_copyright_count=$((no_copyright_count + 1))
-        fi
-    done < <(eval "$cmd")
-
-    if [ $no_copyright_count -gt 0 ]; then
+    if [ -n "$missing_files" ]; then
+        echo "No copyright header found in:"
+        echo "$missing_files"
+        no_copyright_count=$(echo "$missing_files" | wc -l)
         echo "Total files without copyright header: $no_copyright_count"
         return 1
+    else
+        echo "All files have copyright headers."
+        return 0
     fi
-
-    echo "All files have copyright headers."
-    return 0
 }
 
 check_copyright_headers
-RESULT=$?
-
-exit $RESULT
