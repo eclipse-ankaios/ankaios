e2523b005a7603593a36d9107841f72255559d5e
krucod3 <36224699+krucod3@users.noreply.github.com>
2025-01-13 14:05:49 +0000

    Add req linkage

--- common/src/objects/workload_spec.rs
+++ common/src/objects/workload_spec.rs
@@ -562,6 +562,7 @@ mod tests {
 
     // [utest->swdd~common-workload-naming-convention~1]
     // [utest->swdd~common-agent-naming-convention~1]
+    // [utest->swdd~common-access-rules-filter-mask-convention~1]
     #[test]
     fn utest_workload_verify_fields_format_success() {
         let compatible_workload_spec = generate_test_workload_spec();
