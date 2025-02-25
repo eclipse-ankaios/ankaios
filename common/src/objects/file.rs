213911480954604c8c5ef7b54ea15c1779a8a065
Oliver <oliver.klapper@elektrobit.com>
2025-02-25 15:38:09 +0100

    Add utests for conversions in files.rs

--- common/src/objects/file.rs
+++ common/src/objects/file.rs
@@ -92,3 +92,118 @@ pub fn generate_test_rendered_workload_files() -> Vec<File> {
         },
     ]
 }
+
+//////////////////////////////////////////////////////////////////////////////
+//                 ########  #######    #########  #########                //
+//                    ##     ##        ##             ##                    //
+//                    ##     #####     #########      ##                    //
+//                    ##     ##                ##     ##                    //
+//                    ##     #######   #########      ##                    //
+//////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::{Base64Data, Data, File, FileContent};
+    use api::ank_base;
+
+    const MOUNT_POINT_1: &str = "/file.json";
+    const MOUNT_POINT_2: &str = "/binary_file";
+    const TEXT_FILE_CONTENT: &str = "text data";
+    const BASE64_FILE_CONTENT: &str = "base64_data";
+
+    #[test]
+    fn utest_convert_text_file_to_ankaios() {
+        let proto_file = ank_base::File {
+            mount_point: MOUNT_POINT_1.to_owned(),
+            file_content: Some(ank_base::file::FileContent::Data(
+                TEXT_FILE_CONTENT.to_owned(),
+            )),
+        };
+
+        let result = File::try_from(proto_file);
+
+        assert!(result.is_ok());
+        let ankaios_file = result.unwrap();
+        assert_eq!(ankaios_file.mount_point, MOUNT_POINT_1);
+        assert_eq!(
+            ankaios_file.file_content,
+            FileContent::Data(Data {
+                data: TEXT_FILE_CONTENT.to_owned()
+            })
+        );
+    }
+
+    #[test]
+    fn utest_convert_binary_file_to_ankaios() {
+        let proto_binary_file = ank_base::File {
+            mount_point: MOUNT_POINT_2.to_owned(),
+            file_content: Some(ank_base::file::FileContent::BinaryData(
+                BASE64_FILE_CONTENT.to_owned(),
+            )),
+        };
+
+        let result = File::try_from(proto_binary_file);
+
+        assert!(result.is_ok());
+        let ankaios_file = result.unwrap();
+        assert_eq!(ankaios_file.mount_point, MOUNT_POINT_2);
+        assert_eq!(
+            ankaios_file.file_content,
+            FileContent::BinaryData(Base64Data {
+                base64_data: BASE64_FILE_CONTENT.to_owned()
+            })
+        );
+    }
+
+    #[test]
+    fn utest_convert_proto_file_to_ankaios_no_file_content_fails() {
+        let proto_binary_file = ank_base::File {
+            mount_point: MOUNT_POINT_2.to_owned(),
+            file_content: None,
+        };
+
+        let result = File::try_from(proto_binary_file);
+
+        assert_eq!(result, Err("Missing field 'fileContent'".to_string()));
+    }
+
+    #[test]
+    fn utest_convert_text_file_to_proto() {
+        let text_file = File {
+            mount_point: MOUNT_POINT_1.to_owned(),
+            file_content: FileContent::Data(Data {
+                data: TEXT_FILE_CONTENT.to_owned(),
+            }),
+        };
+
+        let file_as_proto = ank_base::File::from(text_file);
+
+        assert_eq!(file_as_proto.mount_point, MOUNT_POINT_1);
+        assert!(file_as_proto.file_content.is_some());
+        let proto_text_file_content = file_as_proto.file_content.unwrap();
+        assert_eq!(
+            proto_text_file_content,
+            ank_base::file::FileContent::Data(TEXT_FILE_CONTENT.to_owned())
+        );
+    }
+
+    #[test]
+    fn utest_convert_binary_file_to_proto() {
+        let binary_file = File {
+            mount_point: MOUNT_POINT_2.to_owned(),
+            file_content: FileContent::BinaryData(Base64Data {
+                base64_data: BASE64_FILE_CONTENT.to_owned(),
+            }),
+        };
+
+        let binary_file_as_proto = ank_base::File::from(binary_file);
+
+        assert_eq!(binary_file_as_proto.mount_point, MOUNT_POINT_2);
+        assert!(binary_file_as_proto.file_content.is_some());
+        let proto_binary_file_content = binary_file_as_proto.file_content.unwrap();
+        assert_eq!(
+            proto_binary_file_content,
+            ank_base::file::FileContent::BinaryData(BASE64_FILE_CONTENT.to_owned())
+        );
+    }
+}
