81d1afd2105152fbbb140dc33957c73df9bfabbf
HorjuRares <horjurares@gmail.com>
2025-03-20 17:09:42 +0000

    Update paths for configuration files archive

--- tools/create_configs.sh
+++ tools/create_configs.sh
@@ -7,8 +7,9 @@ CONFIG_FILES_NAME_BASE="ankaios_configs"
 SERVER_CONFIG_FILE="${ROOT_DIR}/server/config/ank-server.conf"
 AGENT_CONFIG_FILE="${ROOT_DIR}/agent/config/ank-agent.conf"
 ANK_CONFIG_FILE="${ROOT_DIR}/ank/config/ank.conf"
+DIST_DIR="${ROOT_DIR}/dist"
 
-cd "${ROOT_DIR}"
+cd "${DIST_DIR}"
 tar -cvzf "${CONFIG_FILES_NAME_BASE}".tar.gz \
     -C "$(dirname "$SERVER_CONFIG_FILE")" "$(basename "$SERVER_CONFIG_FILE")" \
     -C "$(dirname "$AGENT_CONFIG_FILE")" "$(basename "$AGENT_CONFIG_FILE")" \
