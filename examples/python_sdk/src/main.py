a537b1025632f685c4a859f8a7234ee75bd6e1e9
Tomuta Gabriel <gabriel.tomuta@elektrobit.com>
2025-07-22 16:10:45 +0300

    Add python_sdk example for logging

---
+++ examples/python_sdk/src/main.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2025 Elektrobit Automotive GmbH
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+This example demonstrates how to request logs from workloads running on the system.
+It connects to the Ankaios control interface, retrieves the names of workloads,
+and requests logs for those workloads that match a specific name ("screamer" in this case).
+It then prints the logs received from the log campaign for a limited time (10 seconds).
+
+How to run:
+$ ./run_example.sh python_sdk --manifest-file logging_manifest.yaml
+"""
+
+from ankaios_sdk import Ankaios, AnkaiosException
+import sys, signal
+import time
+
+# Create a new Ankaios object.
+# The connection to the control interface is automatically done at this step.
+# The Ankaios class supports context manager syntax:
+with Ankaios() as ankaios:
+
+    def signal_handler(sig, frame):
+        ankaios.disconnect()
+        sys.exit(0)
+
+    # Add a SIGTERM handler to allow a clean shutdown
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    try:
+        # Get the workload instance names of the workloads running on the system
+        workload_states_list = ankaios.get_workload_states().get_as_list()
+        workload_names = [
+            workload_state.workload_instance_name
+            for workload_state in workload_states_list
+            if workload_state.workload_instance_name.workload_name == "screamer"
+        ]
+
+        # Request the logs of the workloads
+        print("Requesting logs")
+        log_campaign = ankaios.request_logs(
+            workload_names=workload_names,
+            follow=True,
+        )
+
+        # Check what workloads were accepted for logging
+        print("Accepted workload names for logging: {}"
+              .format([
+                  instance_name.workload_name
+                  for instance_name in log_campaign.accepted_workload_names
+                  ]))
+
+        start_time = time.time()
+        while time.time() - start_time < 10:
+            # Get the logs from the queue
+            log = log_campaign.queue.get()
+
+            # Print the logs
+            print(str(log))
+
+    # Catch the AnkaiosException in case something went wrong with apply_workload
+    except AnkaiosException as e:
+        print("Ankaios Exception occurred: ", e)
+
+    # Stop receiving logs
+    print("Stopping log campaign...")
+    ankaios.stop_receiving_logs(log_campaign)
+    print("Done.")
--- examples/run_example.sh
+++ examples/run_example.sh
@@ -5,12 +5,58 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ANKAIOS_SERVER_SOCKET="0.0.0.0:25551"
 ANKAIOS_SERVER_URL="http://${ANKAIOS_SERVER_SOCKET}"
 DEFAULT_ANKAIOS_BIN_PATH="/usr/local/bin"
+DEFAULT_MANIFEST_FILE="startupState.yaml"
 
 display_usage() {
-    echo -e "Usage: $0 EXAMPLE"
-    echo -e "Build and run a control interface example."
-    echo -e "  EXAMPLE: subfolder of the example, e.g. rust_control_interface"
-    echo -e "Optionally, set environment variable for alternative Ankaios executable path: export ANK_BIN_DIR=/path/to/ankaios/executables, if not set default path: '${DEFAULT_ANKAIOS_BIN_PATH}'"
+    echo -e "Usage: $0 <name> [--manifest-file <manifest_file>] [--ankaios-bin-dir <path>] [additional podman build args]"
+    echo -e ""
+    echo -e "Build and run a control interface application."
+    echo -e "  name:              subfolder of the example, e.g. rust_control_interface"
+    echo -e "  --help | -h:       display this help message and exit."
+    echo -e "  --manifest-file:   specify a manifest file from the config directory to use. Default is '${DEFAULT_MANIFEST_FILE}'."
+    echo -e "  --ankaios-bin-dir: specify the path to the Ankaios executables. This can be set using the ANK_BIN_DIR env var as well. Default is '${DEFAULT_ANKAIOS_BIN_PATH}'."
+    echo -e "Optionally, any additional arguments will be passed to the podman build command."
+    echo -e ""
+    echo -e "Example: $0 rust_control_interface --manifest-file my_manifest.yaml --ankaios-bin-dir /path/to/ankaios/bin --target x86_64-unknown-linux-gnu"
+}
+
+parse_args() {
+    # Default values
+    MANIFEST_FILE=${DEFAULT_MANIFEST_FILE}
+    ANK_BIN_DIR=${ANK_BIN_DIR:-$DEFAULT_ANKAIOS_BIN_PATH}
+    PODMAN_BUILD_ARGS=""
+
+    EXAMPLE=$1
+    shift
+
+    if [ "$EXAMPLE" = "--help" ] || [ "$EXAMPLE" = "-h" ]; then
+        display_usage
+        exit 0
+    fi
+
+    # Parse arguments
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --manifest-file)
+                MANIFEST_FILE="$2"
+                shift 2
+                ;;
+            --ankaios-bin-dir)
+                ANK_BIN_DIR="$2"
+                shift 2
+                ;;
+            --help|-h)
+                display_usage
+                exit 0
+                ;;
+            *)
+                PODMAN_BUILD_ARGS="${PODMAN_BUILD_ARGS} $1"
+                shift
+                ;;
+        esac
+    done
+
+    ANK_BIN_DIR=${ANK_BIN_DIR%/}  # Remove trailing slash
 }
 
 
@@ -20,7 +66,7 @@ run_ankaios() {
 
   # Start the Ankaios server
   echo "Starting Ankaios server located in '${ANK_BIN_DIR}'."
-  RUST_LOG=debug ${ANK_BIN_DIR}/ank-server --insecure --startup-config ${SCRIPT_DIR}/config/startupState.yaml --address ${ANKAIOS_SERVER_SOCKET} > ${ANKAIOS_LOG_DIR}/ankaios-server.log 2>&1 &
+  RUST_LOG=debug ${ANK_BIN_DIR}/ank-server --insecure --startup-config ${SCRIPT_DIR}/config/${MANIFEST_FILE} --address ${ANKAIOS_SERVER_SOCKET} > ${ANKAIOS_LOG_DIR}/ankaios-server.log 2>&1 &
 
   sleep 2
   # Start an Ankaios agent
@@ -39,11 +85,7 @@ if [ -z $1 ]; then
   exit 1
 fi
 
-if [ -z ${ANK_BIN_DIR} ]; then
-  ANK_BIN_DIR=${DEFAULT_ANKAIOS_BIN_PATH}
-fi
-
-ANK_BIN_DIR=${ANK_BIN_DIR%/} # remove trailing / if there is one
+parse_args "$@"
 
 if [[ ! -f ${ANK_BIN_DIR}/ank-server || ! -f ${ANK_BIN_DIR}/ank-agent ]]; then
   echo "Failed to build and execute example: no Ankaios executables inside '${ANK_BIN_DIR}'."
@@ -51,8 +93,12 @@ if [[ ! -f ${ANK_BIN_DIR}/ank-server || ! -f ${ANK_BIN_DIR}/ank-agent ]]; then
   exit 2
 fi
 
-echo Build control interface example ...
-podman build "${@:2}" -t control_interface_prod:0.1 -f examples/$1/Dockerfile ${SCRIPT_DIR}/../
+if [ -z "${PODMAN_BUILD_ARGS}" ]; then
+  echo "Build control interface example ..."
+else
+  echo "Build control interface example with: ${PODMAN_BUILD_ARGS} ..."
+fi
+podman build ${PODMAN_BUILD_ARGS} -t control_interface_prod:0.1 -f examples/"${EXAMPLE}"/Dockerfile "${SCRIPT_DIR}"/../
 echo done.
 
 if pgrep -x "ank-server" >/dev/null
