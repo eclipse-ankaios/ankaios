570673add6b2d570c12edfaaa25cea2282e42a97
Christoph Hamm <130038849+christoph-hamm@users.noreply.github.com>
2025-10-10 14:08:07 +0200

    Make changes to gRPC API for supporting events (#573)
    * Make changes to gRPC API for supporting events
    
    Issue-Id: #450
    
    * Fix clippy warnings
    
    Issue-Id: #450
    
    * Fix typo
    
    Co-authored-by: Oliver <42932060+inf17101@users.noreply.github.com>
    
    * Rename oneof variant: completeState -> completeStateResponse
    
    Issue-Id: #450
    
    * In CompleteStateResponse move altered fields to separate message
    
    Issue-Id: #450
    
    * Add EventsCancelRequest and EventsCancelAccepted messages
    
    ---------
    
    Co-authored-by: Oliver <42932060+inf17101@users.noreply.github.com>
    Co-authored-by: Tomuta Gabriel <gabriel.tomuta@elektrobit.com>

--- common/src/to_server_interface.rs
+++ common/src/to_server_interface.rs
@@ -90,6 +90,7 @@ pub trait ToServerInterface {
         request_id: String,
         logs_stop_response: ank_base::LogsStopResponse,
     ) -> Result<(), ToServerError>;
+    async fn event_cancel_request(&self, request_id: String) -> Result<(), ToServerError>;
     async fn goodbye(&self, connection_name: String) -> Result<(), ToServerError>;
     async fn stop(&self) -> Result<(), ToServerError>;
 }
@@ -157,11 +158,7 @@ impl ToServerInterface for ToServerSender {
         Ok(self
             .send(ToServer::Request(commands::Request {
                 request_id,
-                request_content: RequestContent::CompleteStateRequest(
-                    commands::CompleteStateRequest {
-                        field_mask: request_complete_state.field_mask,
-                    },
-                ),
+                request_content: RequestContent::CompleteStateRequest(request_complete_state),
             }))
             .await?)
     }
@@ -208,6 +205,15 @@ impl ToServerInterface for ToServerSender {
             .await?)
     }
 
+    async fn event_cancel_request(&self, request_id: String) -> Result<(), ToServerError> {
+        Ok(self
+            .send(ToServer::Request(commands::Request {
+                request_id,
+                request_content: RequestContent::EventsCancelRequest,
+            }))
+            .await?)
+    }
+
     async fn goodbye(&self, connection_name: String) -> Result<(), ToServerError> {
         Ok(self
             .send(ToServer::Goodbye(commands::Goodbye { connection_name }))
@@ -234,8 +240,8 @@ mod tests {
     use crate::{
         commands::{self, AgentLoadStatus, RequestContent},
         objects::{
-            generate_test_workload_spec, generate_test_workload_state, CpuUsage, ExecutionState,
-            FreeMemory, WorkloadInstanceName,
+            CpuUsage, ExecutionState, FreeMemory, WorkloadInstanceName,
+            generate_test_workload_spec, generate_test_workload_state,
         },
         test_utils::generate_test_complete_state,
         to_server_interface::{ToServer, ToServerInterface},
@@ -273,14 +279,15 @@ mod tests {
         let (tx, mut rx): (ToServerSender, ToServerReceiver) =
             tokio::sync::mpsc::channel(TEST_CHANNEL_CAPA);
 
-        assert!(tx
-            .agent_load_status(AgentLoadStatus {
+        assert!(
+            tx.agent_load_status(AgentLoadStatus {
                 agent_name: AGENT_NAME.to_string(),
                 cpu_usage: CPU_USAGE.clone(),
                 free_memory: FREE_MEMORY.clone(),
             })
             .await
-            .is_ok());
+            .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await,
@@ -316,14 +323,15 @@ mod tests {
 
         let workload1 = generate_test_workload_spec();
         let complete_state = generate_test_complete_state(vec![workload1]);
-        assert!(tx
-            .update_state(
+        assert!(
+            tx.update_state(
                 REQUEST_ID.to_string(),
                 complete_state.clone(),
                 vec![FIELD_MASK.to_string()]
             )
             .await
-            .is_ok());
+            .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
@@ -346,10 +354,11 @@ mod tests {
             tokio::sync::mpsc::channel(TEST_CHANNEL_CAPA);
 
         let workload_state = generate_test_workload_state(WORKLOAD_NAME, ExecutionState::running());
-        assert!(tx
-            .update_workload_state(vec![workload_state.clone()])
-            .await
-            .is_ok());
+        assert!(
+            tx.update_workload_state(vec![workload_state.clone()])
+                .await
+                .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
@@ -367,12 +376,14 @@ mod tests {
 
         let complete_state_request = commands::CompleteStateRequest {
             field_mask: vec![FIELD_MASK.to_string()],
+            subscribe_for_events: false,
         };
         let request_content = RequestContent::CompleteStateRequest(complete_state_request.clone());
-        assert!(tx
-            .request_complete_state(REQUEST_ID.to_string(), complete_state_request)
-            .await
-            .is_ok());
+        assert!(
+            tx.request_complete_state(REQUEST_ID.to_string(), complete_state_request)
+                .await
+                .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
@@ -396,10 +407,11 @@ mod tests {
             until: None,
         };
         let request_content = RequestContent::LogsRequest(logs_request.clone());
-        assert!(tx
-            .logs_request(REQUEST_ID.into(), logs_request)
-            .await
-            .is_ok());
+        assert!(
+            tx.logs_request(REQUEST_ID.into(), logs_request)
+                .await
+                .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
@@ -443,10 +455,11 @@ mod tests {
             }],
         };
 
-        assert!(tx
-            .log_entries_response(REQUEST_ID.into(), logs_response.clone())
-            .await
-            .is_ok());
+        assert!(
+            tx.log_entries_response(REQUEST_ID.into(), logs_response.clone())
+                .await
+                .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
@@ -468,10 +481,11 @@ mod tests {
             }),
         };
 
-        assert!(tx
-            .logs_stop_response(REQUEST_ID.into(), response_content.clone())
-            .await
-            .is_ok());
+        assert!(
+            tx.logs_stop_response(REQUEST_ID.into(), response_content.clone())
+                .await
+                .is_ok()
+        );
 
         assert_eq!(
             rx.recv().await.unwrap(),
