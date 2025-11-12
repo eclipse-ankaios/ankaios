d87a9d1f7a9e6103d123deb71210d504c0c6964c
krucod3 <36224699+krucod3@users.noreply.github.com>
2025-11-12 10:11:49 +0000

    Remove the debug print

--- internal_derive_macros/src/lib.rs
+++ internal_derive_macros/src/lib.rs
@@ -37,19 +37,16 @@ pub fn derive_internal(input: TokenStream) -> TokenStream {
         .into()
 }
 
-
 #[proc_macro_attribute]
 pub fn fix_enum_serialization(attr: TokenStream, item: TokenStream) -> TokenStream {
-
-     assert!(attr.is_empty(), "The fix_enum_serialization attribute does not take any arguments");
-
+    assert!(
+        attr.is_empty(),
+        "The fix_enum_serialization attribute does not take any arguments"
+    );
 
     let input = parse_macro_input!(item as DeriveInput);
-    let result = fix_enum_serialization::fix_prost_enum_serialization(input)
+
+    fix_enum_serialization::fix_prost_enum_serialization(input)
         .unwrap_or_else(syn::Error::into_compile_error)
-        .into();
-
-    println!("Resulting fix_enum_serialization: {result}");
-
-    result
+        .into()
 }
