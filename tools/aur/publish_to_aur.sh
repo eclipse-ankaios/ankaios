966b9ed675ec7dbe36a443021075acfd91eb0f2a
Christoph Hamm <130038849+christoph-hamm@users.noreply.github.com>
2026-05-04 12:12:51 +0200

    Add script for uploading to AUR

---
+++ tools/aur/publish_to_aur.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+AUR_REPO_BASE="${AUR_REPO_BASE:-ssh://aur@aur.archlinux.org}"
+ASSETS=("ank-server.service" "ank-agent.service" "ankaios-cli.install")
+
+if [ -z "${ANKAIOS_VERSION}" ]; then
+    echo "Error: ANKAIOS_VERSION environment variable is not set."
+    exit 1
+fi
+
+tmp_dir="$(mktemp -d)"
+echo $tmp_dir
+#trap 'rm -rf "$tmp_folder"' EXIT
+
+cd "$SCRIPT_DIR"
+for package in ankaios ankaios-bin ankaios-git; do
+    git clone "$AUR_REPO_BASE/$package.git" "$tmp_dir/$package"
+
+    cd "$tmp_dir/$package"
+    rm -rf -- *
+
+    m4 -D ANKAIOS_VERSION="$ANKAIOS_VERSION" "$SCRIPT_DIR/PKGBUILD-$package.m4" > PKGBUILD
+    cp -t ./ "${ASSETS[@]/#/$SCRIPT_DIR/}"
+    updpkgsums
+    makepkg --printsrcinfo > .SRCINFO
+
+    git add PKGBUILD .SRCINFO "${ASSETS[@]}"
+    git commit -a -m "Update version to $ANKAIOS_VERSION" || true
+    git push
+
+    cd "$SCRIPT_DIR"
+done
